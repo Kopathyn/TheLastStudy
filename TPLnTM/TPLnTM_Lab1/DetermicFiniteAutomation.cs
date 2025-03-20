46c8056ff0b685ba5158b90eb1321986ab1583d3
Kopathyn <chemodanov02@gmail.com>
2025-03-20 23:53:14 +0700

    Создание ОПЗ разнесено по отельным функциям, создана новая таблица переходов

--- TPLnTM/TPLnTM_Lab1/DetermicFiniteAutomation.cs
+++ TPLnTM/TPLnTM_Lab1/DetermicFiniteAutomation.cs
@@ -1,4 +1,13 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
+using TPLnTM_Lab1;
+
+public class Transition
+{
+    public string CurrentState;
+    public string CurrentAlphabet;
+    public string StackAction;
+}
 
 public class DeterministicFiniteAutomatonWithStack
 {
@@ -22,6 +31,17 @@ public class DeterministicFiniteAutomatonWithStack
     }
 
     /// <summary>
+    /// Сообщение об ошибке
+    /// </summary>
+    private void ErrorMessage(string state, char symbol)
+    {
+        Console.WriteLine("Автомат завершил работу с ошибкой.\n" +
+            $"Состояние {state}, Символ {symbol}");
+
+        File.WriteAllText(_filePath, "Строка введена некорректно");
+    }
+
+    /// <summary>
     /// Запуск ДМПА
     /// </summary>
     /// <param name="str">Строка</param>
@@ -37,18 +57,21 @@ public class DeterministicFiniteAutomatonWithStack
         for (int i = 0; i < str.Length; i++)
         {
             char symbol = str[i];
+            bool transitionFlag = false;
 
             string matchedAlphabet = MatchSymbol(symbol);
 
             if (matchedAlphabet == null)
             {
-                Console.WriteLine("Автомат завершил работу с ошибкой.\n" +
-                    $"Состояние {state}, Символ {symbol}");
-                File.WriteAllText(_filePath, "Строка введена некорректно");
+                ErrorMessage(state, symbol);
                 return null;
             }
+            else if (matchedAlphabet == _alphabet[7])
+            {
+                transitionFlag = true;
+                continue;
+            }
 
-            bool transitionFlag = false;
             foreach (var transition in _transitionTable[state])
             {
                 if (transition[1] == matchedAlphabet || transition[1] == symbol.ToString())
@@ -58,45 +81,31 @@ public class DeterministicFiniteAutomatonWithStack
 
                     if (transition.Length == 3)
                     {
-                        if (transition[2] == "Push")
+                        string transitionAction = transition[2];
+
+                        switch (transitionAction)
                         {
-                            _stack.Push(symbol);
+                            case "Push":
+                                _stack.Push(symbol);
+                                _rpnCreator.ActionTwo(symbol);
+                                break;
+                            case "Pop":
+                                _stack.Pop();
+                                _rpnCreator.ActionThree();
+                                break;
+                            case "1":
+                                _rpnCreator.ActionOne(symbol);
+                                break;
+                            case "4":
+                                _rpnCreator.ActionFour(symbol);
+                                break;
 
-                            operatorStack.Push(symbol);
-                        }
-
-                        else if (transition[2] == "Pop")
-                        {
-                            _stack.Pop();
-
-                            outputString += " ";
-                            while (operatorStack.Count > 0 && operatorStack.Peek() != '(')
-                                outputString += operatorStack.Pop();
-
-                            if (operatorStack.Count > 0)
-                                operatorStack.Pop();
+                            default:
+                                ErrorMessage(state, symbol);
+                                break;
                         }
 
                         break;
-                    }
-
-                    if (char.IsWhiteSpace(symbol))
-                        break;
-
-                    // Обработка символов в соответствии с алгоритмом
-                    if (char.IsLetterOrDigit(symbol) || symbol == '.')
-                        outputString += symbol;
-
-                    else // Если символ - знак операции
-                    {
-                        outputString += " ";
-
-                        int currentPriority = GetOperatorPriority(symbol);
-
-                        while (operatorStack.Count > 0 && GetOperatorPriority(operatorStack.Peek()) >= currentPriority)
-                            outputString += operatorStack.Pop() + " ";
-
-                        operatorStack.Push(symbol);
                     }
 
                     break;
@@ -105,60 +114,160 @@ public class DeterministicFiniteAutomatonWithStack
 
             if (!transitionFlag)
             {
-                Console.WriteLine("Автомат завершил работу с ошибкой.\n" +
-                    $"Состояние {state}, Символ {symbol}");
-                File.WriteAllText(_filePath, "Строка введена некорректно");
+                ErrorMessage(state, symbol);
                 return null;
             }
         }
 
-        // Извлечение оставшихся операторов из стека в выходную строку
-        while (operatorStack.Count > 0)
-            outputString += " " + operatorStack.Pop();
-
         // Проверка на завершение
         if (_transitionTable[state].Any(transition => transition[0] == "HALT" && _stack.Count == 0))
         {
+            _rpnCreator.ActionFive();
+
             Console.WriteLine("Автомат завершил работу успешно.");
-            return outputString;
+            return _rpnCreator.OutputString;
         }
         else
         {
-            Console.WriteLine("Автомат завершил работу с ошибкой.\n" +
-                    $"Состояние {state}");
-            File.WriteAllText(_filePath, "Строка введена некорректно");
+            ErrorMessage(state, ' ');
             return null;
-        }
-    }
-
-    // Метод для получения приоритета оператора
-    private int GetOperatorPriority(char op)
-    {
-        switch (op)
-        {
-            case '*':
-                return 3; // Наивысший приоритет
-            case '+':
-                return 2; // Средний приоритет
-            default:
-                return 0; // Не оператор
         }
     }
 
     #region PrivateFields
 
+    private static ReversePolishNotation _rpnCreator = new();
+
     private string _filePath;
 
-    // Алфавит автомата в РВ
+    /// <summary>
+    /// Состояния автомата
+    /// </summary>
+    private string[] _states = 
+    {
+        "q0", "q1", "q2", "q3", "q4", "q5", "q6", 
+        "q7", "q8", "q9", "q10", "q11", "q12",
+    }; 
+
+    /// <summary>
+    /// Алфавит автомата в РВ
+    /// </summary>
+    /// <value> 
+    ///   Буквы [0];
+    ///   Цифры [1];
+    ///   Знаки сложения и умножения [2];
+    ///   Скобки [3, 4];
+    ///   "Запятая" [5];
+    ///   Знак равенства [6];
+    ///   Пробел [7];
+    /// </value>
     private static string[] _alphabet =
     {
-        "\\p{L}",     // Буквы (0)
-        "\\d",        // Цифры (1)
-        "[\\+\\*]",   // Знаки сложения и умножения (2)
-        "\\(", "\\)", // Скобки (3, 4)
-        "\\.",        // "Запятая" (5)
-        "\\=",        // Знак равенства (6)
-        "\\s"         // Пробел (7)
+        "\\p{L}",
+        "\\d",
+        "[\\+\\*]",
+        "\\(", "\\)",
+        "\\.",
+        "\\=",
+        "\\s"
+    };
+
+    // Стек(Магазин) автомата
+    private static Stack<char> _stack = new();
+
+    private static Action<char>[] _stackActions =
+    { 
+        x => _stack.Push(x),
+        x => _stack.Pop()
+    };
+
+    private static Action<char>[] _implementedActions =
+    {
+        x => _rpnCreator.ActionOne(x),
+        x => _rpnCreator.ActionTwo(x),
+        x => _rpnCreator.ActionThree(),
+        x => _rpnCreator.ActionFour(x),
+        x => _rpnCreator.ActionFive()
+    };
+
+
+    private Dictionary<Tuple<string, string, Action<char>>, Tuple<string, Action<char>, Action<char>>> _newTransitionTable = new()
+    {
+        // q0
+        { new("q0", _alphabet[0], null), new("q1", null, _implementedActions[0]) },
+        { new("q0", _alphabet[7], null), new("q0", null, null) },
+
+        // q1
+        { new("q1", _alphabet[0], null), new("q1", null, _implementedActions[0]) },
+        { new("q1", _alphabet[1], null), new("q1", null, _implementedActions[0]) },
+        { new("q1", _alphabet[7], null), new("q2", null, null) },
+        { new("q1", _alphabet[6], null), new("q3", null, _implementedActions[3]) },
+
+        // q2
+        { new("q2", _alphabet[7], null), new("q2", null, null) },
+        { new("q2", _alphabet[6], null), new("q3", null, _implementedActions[3]) },
+
+        // q3
+        { new("q3", _alphabet[7], null), new("q3", null, null) },
+        { new("q3", _alphabet[3], _stackActions[0]), new("q3", null, _implementedActions[1]) },
+        { new("q3", _alphabet[1], null), new("q4", null, _implementedActions[0]) },
+        { new("q3", _alphabet[0], null), new("q5", null, _implementedActions[0]) },
+
+        // q4
+        { new("q4", _alphabet[2], null), new("q3", null, _implementedActions[3]) },
+        { new("q4", _alphabet[1], null), new("q4", null, _implementedActions[0]) },
+        { new("q4", _alphabet[4], _stackActions[1]), new("q6", null, _implementedActions[2]) },
+        { new("q4", _alphabet[5], null), new("q7", null, _implementedActions[0]) },
+        { new("q4", "E", null), new("q9", null, _implementedActions[0]) },
+        { new("q4", "e", null), new("q9", null, _implementedActions[0]) },
+        { new("q4", _alphabet[7], null), new("q12", null, null) },
+        { new("q4", "", null), new("HALT", null, _implementedActions[4]) },
+
+        // q5
+        { new("q5", _alphabet[2], null), new("q3", null, _implementedActions[3]) },
+        { new("q5", _alphabet[0], null), new("q5", null, _implementedActions[0]) },
+        { new("q5", _alphabet[1], null), new("q5", null, _implementedActions[0]) },
+        { new("q5", _alphabet[4], _stackActions[1]), new("q6", null, _implementedActions[2]) },
+        { new("q5", _alphabet[7], null), new("q12", null, null) },
+        { new("q5", "", null), new("HALT", null, _implementedActions[4]) },
+
+        // q6
+        { new("q6", _alphabet[2], null), new("q3", null, _implementedActions[3]) },
+        { new("q6", _alphabet[4], _stackActions[1]), new("q6", null, _implementedActions[2]) },
+        { new("q6", _alphabet[7], null), new("q6", null, null) },
+        { new("q6", "", null), new("HALT", null, _implementedActions[4]) },
+
+        // q7
+        { new("q7", _alphabet[1], null), new("q8", null, _implementedActions[0]) },
+
+        // q8
+        { new("q8", _alphabet[2], null), new("q3", null, _implementedActions[3]) },
+        { new("q8", _alphabet[4], _stackActions[1]), new("q6", null, _implementedActions[2]) },
+        { new("q8", _alphabet[1], null), new("q8", null, _implementedActions[0]) },
+        { new("q8", "E", null), new("q9", null, _implementedActions[0]) },
+        { new("q8", "e", null), new("q9", null, _implementedActions[0]) },
+        { new("q8", _alphabet[7], null), new("q12", null, null) },
+        { new("q8", "", null), new("HALT", null, _implementedActions[4]) },
+
+        // q9
+        { new("q9", "+", null), new("q10", null, _implementedActions[0]) },
+        { new("q9", "-", null), new("q10", null, _implementedActions[0]) },
+
+        // q10
+        { new("q10", _alphabet[1], null), new("q11", null, _implementedActions[0]) },
+
+        // q11
+        { new("q11", _alphabet[2], null), new("q3", null, _implementedActions[3]) },
+        { new("q11", _alphabet[4], _stackActions[1]), new("q6", null, _implementedActions[2]) },
+        { new("q11", _alphabet[1], null), new("q11", null, _implementedActions[0]) },
+        { new("q11", _alphabet[7], null), new("q12", null, null) },
+        { new("q11", "", null), new("HALT", null, _implementedActions[4]) },
+
+        // q12
+        { new("q12", _alphabet[2], null), new("q3", null, _implementedActions[3]) },
+        { new("q12", _alphabet[4], _stackActions[1]), new("q6", null, _implementedActions[2]) },
+        { new("q12", _alphabet[7], null), new("q12", null, null) },
+        { new("q12", "", null), new("HALT", null, _implementedActions[4]) }
     };
 
     // Таблица переходов
@@ -168,7 +277,7 @@ public class DeterministicFiniteAutomatonWithStack
             "q0",
             new string[][] 
             {
-                new[] { "q1", _alphabet[0] },
+                new[] { "q1", _alphabet[0], "1" },
                 new[] { "q0", _alphabet[7] }
             }
         },
@@ -176,10 +285,10 @@ public class DeterministicFiniteAutomatonWithStack
             "q1",
             new string[][]
             {
-                new string[] { "q1", _alphabet[0]  },
-                new string[] { "q1", _alphabet[1] },
+                new string[] { "q1", _alphabet[0], "1"},
+                new string[] { "q1", _alphabet[1], "1" },
                 new string[] { "q2", _alphabet[7] },
-                new[] { "q3", _alphabet[6] }
+                new[] { "q3", _alphabet[6], "4" }
             }
         },
         {
@@ -187,7 +296,7 @@ public class DeterministicFiniteAutomatonWithStack
             new string[][]
             {
                 new string[] { "q2", _alphabet[7] },
-                new string[] { "q3", _alphabet[6] }
+                new string[] { "q3", _alphabet[6], "4" }
             }
         },
         {
@@ -196,20 +305,20 @@ public class DeterministicFiniteAutomatonWithStack
             {
                 new[] { "q3", _alphabet[7] },
                 new[] { "q3", _alphabet[3], "Push" },
-                new string[] { "q4", _alphabet[1] },
-                new string[] { "q5", _alphabet[0] }
+                new string[] { "q4", _alphabet[1], "1" },
+                new string[] { "q5", _alphabet[0], "1" }
             }
         },
         {
             "q4",
             new string[][]
             {
-                new[] { "q3", _alphabet[2] },
-                new string[] { "q4", _alphabet[1] },
+                new[] { "q3", _alphabet[2], "4" },
+                new string[] { "q4", _alphabet[1], "1" },
                 new[] { "q6", _alphabet[4], "Pop" },
-                new string[] { "q7", _alphabet[5] },
-                new[] { "q9", "E" },
-                new[] { "q9", "e"},
+                new string[] { "q7", _alphabet[5], "1" },
+                new[] { "q9", "E", "1" },
+                new[] { "q9", "e", "1" },
                 new[] { "q12", _alphabet[7] },
                 new string[] { "HALT", "" }
             }
@@ -218,9 +327,9 @@ public class DeterministicFiniteAutomatonWithStack
             "q5",
             new string[][]
             {
-                new[] { "q3", _alphabet[2] },
-                new[] { "q5",  _alphabet[0] },
-                new[] { "q5", _alphabet[1] },
+                new[] { "q3", _alphabet[2], "4" },
+                new[] { "q5",  _alphabet[0], "1" },
+                new[] { "q5", _alphabet[1] , "1" },
                 new[] { "q6", _alphabet[4], "Pop" },
                 new[] { "q12", _alphabet[7] },
                 new string[] { "HALT", "" }
@@ -230,7 +339,7 @@ public class DeterministicFiniteAutomatonWithStack
             "q6",
             new string[][] 
             {
-                new[] { "q3", _alphabet[2] },
+                new[] { "q3", _alphabet[2], "4" },
                 new string[] { "q6", _alphabet[4], "Pop" },
                 new[] { "q6", _alphabet[7] },
                 new string[] { "HALT", "" }
@@ -240,18 +349,18 @@ public class DeterministicFiniteAutomatonWithStack
             "q7",
             new string[][]
             {
-                new string[] { "q8", _alphabet[1] }
+                new string[] { "q8", _alphabet[1], "1" }
             }
         },
         {
             "q8",
             new string[][]
             {
-                new[] { "q3", _alphabet[2] },
+                new[] { "q3", _alphabet[2], "4" },
                 new[] { "q6", _alphabet[4], "Pop" },
-                new[] { "q8", _alphabet[1] },
-                new[] { "q9", "E" },
-                new[] { "q9", "e" },
+                new[] { "q8", _alphabet[1], "1" },
+                new[] { "q9", "E" , "1" },
+                new[] { "q9", "e", "1" },
                 new[] { "q12", _alphabet[7] },
                 new string[] { "HALT", "" }
             }
@@ -260,24 +369,24 @@ public class DeterministicFiniteAutomatonWithStack
             "q9",
             new string[][]
             {
-                new[] { "q10", "+"},
-                new[] { "q10", "-"}
+                new[] { "q10", "+", "1"}, //?
+                new[] { "q10", "-", "1" }
             }
         },
         {
             "q10",
             new string[][]
             {
-                new[] { "q11", _alphabet[1] }
+                new[] { "q11", _alphabet[1], "1" }
             }
         },
         {
             "q11",
             new string[][]
             {
-                new[] { "q3", _alphabet[2] },
+                new[] { "q3", _alphabet[2], "4" },
                 new[] { "q6", _alphabet[4], "Pop" },
-                new[] { "q11", _alphabet[1] },
+                new[] { "q11", _alphabet[1], "1" },
                 new[] { "q12", _alphabet[7] },
                 new string[] { "HALT", "" }
             }
@@ -286,7 +395,7 @@ public class DeterministicFiniteAutomatonWithStack
             "q12",
             new string[][]
             {
-                new[] { "q3", _alphabet[2] },
+                new[] { "q3", _alphabet[2], "4" },
                 new[] { "q6", _alphabet[4], "Pop" },
                 new[] { "q12", _alphabet[7] },
                 new string[] { "HALT", "" }
@@ -294,8 +403,6 @@ public class DeterministicFiniteAutomatonWithStack
         }
     };
 
-    // Стек(Магазин) автомата
-    private Stack<char> _stack = new();
 
     #endregion
 }
---
+++ TPLnTM/TPLnTM_Lab1/ReversePolishNotation.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLnTM_Lab1
+{
+    public class ReversePolishNotation
+    {
+        public ReversePolishNotation() 
+        { 
+            _outputString = null; _operatorStack = new(); 
+        }
+
+        /// <summary>
+        /// Добавление части операнда в строку
+        /// </summary>
+        public void ActionOne(char symbol) => _outputString += (symbol);
+
+        /// <summary>
+        /// Добавление открывающей скобки в стек
+        /// </summary>
+        /// <param name="symbol"></param>
+        public void ActionTwo(char openBracket) => _operatorStack.Push(openBracket);
+
+        /// <summary>
+        /// Обработка закрывающей скобки
+        /// </summary>
+        public void ActionThree()
+        {
+            _outputString += " ";
+
+            while (_operatorStack.Count > 0 && _operatorStack.Peek() != '(')
+                _outputString += _operatorStack.Pop();
+
+            if (_operatorStack.Count > 0)
+                _operatorStack.Pop();
+        }
+
+        /// <summary>
+        /// Обработка оператора
+        /// </summary>
+        /// <param name="symbol"></param>
+        public void ActionFour(char symbol)
+        {
+            _outputString += " "; 
+            
+            int currentPriority = GetOperatorPriority(symbol);
+
+            while (_operatorStack.Count > 0 && GetOperatorPriority(_operatorStack.Peek()) >= currentPriority)
+                _outputString += _operatorStack.Pop() + " ";
+
+            _operatorStack.Push(symbol);
+        }
+
+        /// <summary>
+        /// Завершение работы
+        /// </summary>
+        public void ActionFive()
+        {
+            while (_operatorStack.Count > 0)
+                _outputString += " " + _operatorStack.Pop();
+        }
+
+        /// <summary>
+        /// Метод для получения приоритета оператора
+        /// </summary>
+        /// <param name="op">Оператор</param>
+        /// <returns>Приоритет оператора</returns>
+        private int GetOperatorPriority(char op)
+        {
+            switch (op)
+            {
+                case '*':
+                    return 3; // Наивысший приоритет
+                case '+':
+                    return 2; // Средний приоритет
+                default:
+                    return 0; // Не оператор
+            }
+        }
+
+        private Stack<char> _operatorStack;
+        private string _outputString;
+
+        public string OutputString { get { return _outputString; } }
+    }
+}
